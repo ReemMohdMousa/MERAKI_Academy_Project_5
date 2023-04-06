666cab22b2f41b6f7ba77079cdbbb9bd5c38a6a4
AyaShiab <shiab.ayah@gmail.com>
2023-04-06 13:54:20 +0300

    intializing posts files

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const db=require("./models/db")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Import Routers
+
+
 app.use(cors());
 app.use(express.json());
+
+// Routes Middleware
+
 
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
