62a01b99a9a3f196786a7f4d4774d40de68e5c73
ReemMohdMousa <reem.mohd.mousa@gmail.com>
2023-04-06 23:07:36 +0300

    roles

--- backend/routes/role.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {createNewRole, createNewRolePermission} = require("../controllers/role")
-
-const rolesRouter = express.Router();
-rolesRouter.post("/", createNewRole)
-rolesRouter.post("/role_permission", createNewRolePermission);
-
-module.exports = rolesRouter;
