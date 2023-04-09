575e25a81456abf83b252585c312a5ac62e0653e
ReemMohdMousa <reem.mohd.mousa@gmail.com>
2023-04-09 23:50:49 +0300

    done

--- backend/controllers/follow.js
+++ backend/controllers/follow.js
@@ -2,7 +2,7 @@ const { pool } = require("../models/db");
 
 const followUser = (req, res) => {};
 
-const UnfollowUser = (req, res) => {};
+const unFollowUser = (req, res) => {};
 
 const getAllfollowersByUserId = (req, res) => {};
 
@@ -10,7 +10,7 @@ const getAllfollowingsByUserId = (req, res) => {};
 
 module.exports = {
   followUser,
-  UnfollowUser,
+  unFollowUser,
   getAllfollowersByUserId,
   getAllfollowingsByUserId,
 };
