3cba6bf4a7e327790104e95cd4ccac922bf669f9
AyaShiab <shiab.ayah@gmail.com>
2023-04-09 18:41:24 +0300

    seperate likes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,13 @@
 const express = require("express");
 
 const { register, login,checkGoogleUser, addLike, getLikesByUser, getLikesByPost, removeLike } = require("../controllers/user");
-const authentication = require("../middlewares/authentication");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/register", register);
 usersRouter.post("/login", login);
 usersRouter.post("/google", checkGoogleUser);
-usersRouter.post("/like", authentication,addLike);
-usersRouter.get("/like", authentication,getLikesByUser);
-usersRouter.get("/like/:id",getLikesByPost);
-usersRouter.delete("/like/:id",authentication,removeLike);
+
 
 
 module.exports = usersRouter;
