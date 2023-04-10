62532a62cb51799cc64dde5006a65d9fdd837d76
AyaShiab <shiab.ayah@gmail.com>
2023-04-11 01:05:25 +0300

    login p3

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -8,84 +8,169 @@ import { useNavigate } from "react-router-dom";
 import { setLogin, setUserId } from "../redux/reducers/auth";
 import { GoogleLogin } from "@react-oauth/google";
 
-
 import {
-    MDBBtn,
-    MDBContainer,
-    MDBRow,
-    MDBCol,
-    MDBCard,
-    MDBCardBody,
-    MDBInput,
-    MDBIcon
-  } from 'mdb-react-ui-kit';
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+} from "mdb-react-ui-kit";
 const Login = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-  
-    const state = useSelector((state) => {
-      return {
-        auth: state.auth,
-      };
-    });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [password, setPassword] = useState("");
-     
-const login =()=>{}
+  const state = useSelector((state) => {
+    return {
+      auth: state.auth,
+    };
+  });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+
+  const login = () => {
+    axios
+      .post("http://localhost:5000/users/login", {
+        email,
+        password,
+      })
+      .then((result) => {
+        localStorage.setItem("token", result.data.token);
+        localStorage.setItem("userId", result.data.userId);
+        localStorage.setItem("isLoggedIn", true);
+        dispatch(setLogin(result.data.token));
+        dispatch(setUserId(result.data.userId));
+      })
+      .catch((error) => {
+        setShow(true);
+        setMessage(error.response.data.message);
+      });
+  };
+ 
+  useEffect(() => {
+    if (state.auth.isLoggedIn) {
+      navigate("/home");
+    }
+  });
+  const loginGoogle=(result)=>{
+    const { credential, clientId } = result;
+    axios
+      .post("http://localhost:5000/users/google", {
+        credential,
+        clientId,
+      })
+      .then((res) => {
+        const { family_name, email } = res.data;
+        const fakePass = family_name + 123456;
+
+        axios
+        .post("http://localhost:5000/users/login", {
+          email,
+          password: fakePass,
+        })
+        .then((result) => {
+          localStorage.setItem("token", result.data.token);
+          localStorage.setItem("userId", result.data.userId);
+          localStorage.setItem("isLoggedIn", true);
+          dispatch(setLogin(result.data.token));
+          dispatch(setUserId(result.data.userId));
+        })
+        .catch((err) => {
+          setShow(true);
+          setMessage(err.response.data.message);
+        });
+          
+      });
+  }
   return (
-    <div className='cont'> 
-    <MDBContainer fluid>
+    <div className="cont">
+      <MDBContainer fluid>
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard
+              className="bg-dark text-white my-5 mx-auto"
+              style={{ borderRadius: "1rem", maxWidth: "400px" }}
+            >
+              <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+                <h2 className="fw-bold mb-2">Sign to your account</h2>
+                <br />
+                <br />
 
-    <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-      <MDBCol col='12'>
+                <MDBInput
+                  wrapperClass="mb-4 mx-5 w-100"
+                  labelClass="text-white"
+                  label="Email address"
+                  id="formControlLg"
+                  type="email"
+                  size="lg"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MDBInput
+                  wrapperClass="mb-4 mx-5 w-100"
+                  labelClass="text-white"
+                  label="Password"
+                  id="formControlLg1"
+                  type="password"
+                  size="lg"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-        <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
-          <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+                <MDBBtn
+                  outline
+                  className="mx-2 px-5"
+                  color="white"
+                  size="lg"
+                  onClick={(e) => {
+                    login(e);
+                  }}
+                >
+                  Login
+                </MDBBtn>
 
-            <h2 className="fw-bold mb-2">Sign to your account</h2>
-        <br/>
-        <br/>
+                <br />
+                <br />
 
+                <div>
+                  <p className="mb-0">
+                    Don't have an account?{" "}
+                    <a href="#!" class="text-white-50 fw-bold">
+                      Sign Up
+                    </a>
+                  </p>
+                </div>
+                <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Message</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          <GoogleLogin
+            width={"90000px"}
+            theme={"filled_black"}
+            size={"large"}
+            onSuccess={loginGoogle}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
+  );
+};
 
-            <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address' id='formControlLg' type='email' size="lg"  onChange={(e) => setEmail(e.target.value)}/>
-            <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)}/>
-
-            
-            <MDBBtn outline className='mx-2 px-5' color='white' size='lg' onClick={(e) => {
-              login(e);
-            }}>
-              Login
-            </MDBBtn>
-
-            <div className='d-flex flex-row mt-3 mb-5'>
-              <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                <MDBIcon fab icon='facebook-f' size="lg"/>
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                <MDBIcon fab icon='twitter' size="lg"/>
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                <MDBIcon fab icon='google' size="lg"/>
-              </MDBBtn>
-            </div>
-
-            <div>
-              <p className="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a></p>
-
-            </div>
-          </MDBCardBody>
-        </MDBCard>
-
-      </MDBCol>
-    </MDBRow>
-
-  </MDBContainer>
-  </div>
-  )
-}
-
-export default Login
+export default Login;
