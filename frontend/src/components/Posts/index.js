d70349699256ea44ac2318ba73ecd7bfa652dde0
AyaShiab <shiab.ayah@gmail.com>
2023-04-24 23:26:16 +0300

    handle profile css p2

--- frontend/src/components/Posts/index.js
+++ frontend/src/components/Posts/index.js
@@ -71,14 +71,16 @@ const Posts = ({ post, firstname, lastname }) => {
               </div>
             </div>
             <Dropdown>
-              <Dropdown.Toggle id="dropdown-basic">
+              <Dropdown.Toggle
+                variant="light"
+                id="dropdown-basic"
+                style={{ backgroundColor: "inherit" }}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
-                  fill="currentColor"
                   className="bi bi-three-dots"
-                  viewBox="0 0 16 16"
                   onClick={() => {}}
                 >
                   <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
