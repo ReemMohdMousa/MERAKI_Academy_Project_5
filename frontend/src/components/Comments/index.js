52783d1b8ae89c196d40552d09e92f66a8cf7f5d
Tasneem Gharaibeh <tasnim.gharaibeh@gmail.com>
2023-04-27 23:42:46 +0300

    comments

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -45,7 +45,7 @@ const Comments = ({ id, firstname, lastname, socket }) => {
   const[text,setText]=useState()
   function handleOnEnter(text) {}
   const [nemcomment, setNewComment] = useState({});
-
+const[openReplay,setOpenReply]=useState(false)
 
   const { userinfo, token, userId, posts } = useSelector((state) => {
     return {
@@ -355,42 +355,7 @@ const Comments = ({ id, firstname, lastname, socket }) => {
                                       </span>
                                     </p>
 
-                                    {/* <Dropdown>
-                                      <Dropdown.Toggle id="dropdown-basic">
-                                        <svg
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          width="16"
-                                          height="16"
-                                          fill="currentColor"
-                                          className="bi bi-three-dots"
-                                          viewBox="0 0 16 16"
-                                          onClick={() => {}}
-                                        >
-                                          <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-                                        </svg>
-                                      </Dropdown.Toggle>
 
-                                      <Dropdown.Menu>
-                                        <Dropdown.Item
-                                          onClick={() => {
-                                            setShowEdit(true);
-                                          }}
-                                        >
-                                          Edit
-                                        </Dropdown.Item>
-
-                                        <Dropdown.Item
-                                          onClick={() => {
-                                            deleteComment(
-                                              id,
-                                              element.comment_id
-                                            );
-                                          }}
-                                        >
-                                          Delete{" "}
-                                        </Dropdown.Item>
-                                      </Dropdown.Menu>
-                                    </Dropdown> */}
                                      <Dropdown>
               <Dropdown.Toggle
                 variant="light"
@@ -493,6 +458,20 @@ const Comments = ({ id, firstname, lastname, socket }) => {
                                           </span>
                                         </p>
                                       </div>
+                                     
+                                      <button
+                                        onClick={() => {
+                                          createNestedComment(
+                                            element.post_id,
+                                            element.comment_id
+                                          );
+                                        }}
+                                      >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
+  <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>
+</svg>
+                                      </button>
+
                                       <MDBInput
                                         style={{ height: "40px" }}
                                         wrapperClass="mb-4"
@@ -508,16 +487,8 @@ const Comments = ({ id, firstname, lastname, socket }) => {
                                           });
                                         }}
                                       />
-                                      <button
-                                        onClick={() => {
-                                          createNestedComment(
-                                            element.post_id,
-                                            element.comment_id
-                                          );
-                                        }}
-                                      >
-                                        reply
-                                      </button>
+
+
                                     </div>
                                   </div>
                                 </div>
