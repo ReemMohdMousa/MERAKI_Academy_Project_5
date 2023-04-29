3be48eac2f4914adfcac8e2d446eead7a92e0caa
ReemMohdMousa <reem.mohd.mousa@gmail.com>
2023-04-29 17:00:28 +0300

    debugging + style

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -446,91 +446,171 @@ const Comments = ({ id, firstname, lastname, socket }) => {
                                           </div>
                                         </div>
                                       </div>
-                                      {openReplay && (
-                                        <div className="d-flex flex-start mt-4">
-                                          <a className="me-3" href="#">
-                                            <MDBCardImage
-                                              className="rounded-circle shadow-1-strong me-3"
-                                              src={
-                                                userinfo && userinfo.avatar
-                                                  ? userinfo.avatar
-                                                  : "https://png.pngtree.com/png-clipart/20210613/original/pngtree-gray-silhouette-avatar-png-image_6404679.jpg"
-                                              }
-                                              alt="avatar"
-                                              width="65"
-                                              height="65"
-                                            />
-                                          </a>
 
-                                          <div className="flex-grow-1 flex-shrink-1">
-                                            <div>
-                                              <div className="d-flex justify-content-between align-items-center">
-                                                <p className="mb-1">
-                                                  {userinfo.firstname}{" "}
-                                                  {userinfo.lastname}
-                                                  <br></br>{" "}
-                                                </p>
-                                              </div>
+                                      <div>
+                                        {openReplay && (
+                                          <div className="d-flex flex-start mt-4">
+                                            <a className="me-3" href="#">
+                                              <MDBCardImage
+                                                className="rounded-circle shadow-1-strong me-3"
+                                                src={
+                                                  userinfo && userinfo.avatar
+                                                    ? userinfo.avatar
+                                                    : "https://png.pngtree.com/png-clipart/20210613/original/pngtree-gray-silhouette-avatar-png-image_6404679.jpg"
+                                                }
+                                                alt="avatar"
+                                                width="65"
+                                                height="65"
+                                              />
+                                            </a>
 
-                                              <div className="nested-input-btn">
-                                                <div className="nested-input">
-                                                  <input
-                                                    className="nested-input"
-                                                    placeholder="replay the comment..."
-                                                    type="text"
-                                                    onChange={(e) => {
-                                                      setNewNested(
-                                                        e.target.value
-                                                      );
-                                                    }}
-                                                  />
+                                            <div className="flex-grow-1 flex-shrink-1">
+                                              <div>
+                                                <div className="d-flex justify-content-between align-items-center">
+                                                  <p className="mb-1">
+                                                    {userinfo.firstname}{" "}
+                                                    {userinfo.lastname}
+                                                    <br></br>{" "}
+                                                  </p>
                                                 </div>
 
-                                                <div>
-                                                  <button
-                                                    className="submit-nested-btn"
-                                                    onClick={() => {
-                                                      console.log(
-                                                        element.post_id,
-                                                        element.comment_id
-                                                      );
-                                                      createNestedComment(
-                                                        element.post_id,
-                                                        element.comment_id
-                                                      );
-                                                    }}
-                                                  >
-                                                    <svg
-                                                      xmlns="http://www.w3.org/2000/svg"
-                                                      width="16"
-                                                      height="16"
-                                                      fill="currentColor"
-                                                      class="bi bi-reply"
-                                                      viewBox="0 0 16 16"
+                                                <div className="nested-input-btn">
+                                                  <div className="nested-input">
+                                                    <input
+                                                      className="nested-input"
+                                                      placeholder="replay the comment..."
+                                                      type="text"
+                                                      onChange={(e) => {
+                                                        setNewNested(
+                                                          e.target.value
+                                                        );
+                                                      }}
+                                                    />
+                                                  </div>
+
+                                                  <div>
+                                                    <button
+                                                      className="submit-nested-btn"
+                                                      onClick={() => {
+                                                        console.log(
+                                                          element.post_id,
+                                                          element.comment_id
+                                                        );
+                                                        createNestedComment(
+                                                          element.post_id,
+                                                          element.comment_id
+                                                        );
+                                                      }}
                                                     >
-                                                      <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z" />
-                                                    </svg>
-                                                  </button>
+                                                      <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="16"
+                                                        height="16"
+                                                        fill="currentColor"
+                                                        class="bi bi-reply"
+                                                        viewBox="0 0 16 16"
+                                                      >
+                                                        <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z" />
+                                                      </svg>
+                                                    </button>
+                                                  </div>
                                                 </div>
                                               </div>
                                             </div>
                                           </div>
+                                        )}
+                                        <div className="render-nested">
+                                          {allnested &&
+                                            openReplay &&
+                                            allnested.map((nestedComm) => {
+                                              console.log(nestedComm);
+                                              return (
+                                                <div
+                                                  className="d-flex flex-start mt-4"
+                                                  key={
+                                                    nestedComm.nestedcomments_id
+                                                  }
+                                                >
+                                                  <MDBCardImage
+                                                    className="rounded-circle shadow-1-strong me-3"
+                                                    src={
+                                                      element.avatar
+                                                        ? element.avatar
+                                                        : "https://png.pngtree.com/png-clipart/20210613/original/pngtree-gray-silhouette-avatar-png-image_6404679.jpg"
+                                                    }
+                                                    alt="avatar"
+                                                    width="65"
+                                                    height="65"
+                                                  />
 
-                                          <div className="render-nested">
-                                            {allnested &&
-                                              allnested.map((nestedComm) => {
-                                                console.log(nestedComm);
-                                                return (
                                                   <div>
-                                                    <p className="small mb-0">
-                                                      {nestedComm.content}
-                                                    </p>
+                                                    <div>
+                                                      <div className="d-flex justify-content-between align-items-center">
+                                                        <div className="mb-1">
+                                                          <div>
+                                                            {`${nestedComm.firstname}   ${nestedComm.lastname}`}
+                                                          </div>
+                                                          <div>
+                                                            <span
+                                                              className="small"
+                                                              style={{
+                                                                color: "gray",
+                                                              }}
+                                                            >
+                                                              {moment(
+                                                                `${nestedComm.created_at}`
+                                                              ).fromNow()}
+                                                            </span>
+                                                          </div>
+                                                        </div>
+
+                                                        <div>
+                                                          <Dropdown>
+                                                            <Dropdown.Toggle
+                                                              variant="light"
+                                                              id="dropdown-basic"
+                                                              style={{
+                                                                backgroundColor:
+                                                                  "inherit",
+                                                              }}
+                                                            >
+                                                              <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                width="16"
+                                                                height="16"
+                                                                className="bi bi-three-dots"
+                                                                onClick={() => {}}
+                                                              >
+                                                                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                                                              </svg>
+                                                            </Dropdown.Toggle>
+
+                                                            <Dropdown.Menu>
+                                                              <Dropdown.Item>
+                                                                Edit
+                                                              </Dropdown.Item>
+                                                              <Dropdown.Item>
+                                                                Delete
+                                                              </Dropdown.Item>
+                                                            </Dropdown.Menu>
+                                                          </Dropdown>
+                                                        </div>
+                                                      </div>
+
+                                                      <div>
+                                                        {nestedComm.content && (
+                                                          <p className="small mb-0">
+                                                            {nestedComm.content}
+                                                          </p>
+                                                        )}
+                                                      </div>
+                                                    </div>
                                                   </div>
-                                                );
-                                              })}
-                                          </div>
+                                                </div>
+                                              );
+                                            })}
                                         </div>
-                                      )}
+                                      </div>
 
                                       {showEdit ? (
                                         <UpdateComment
